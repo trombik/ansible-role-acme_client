d76a2197471c9b8c7025c41e3ecf91ee04ca95ea
Tomoyuki Sakurai <y@trombik.org>
2020-05-04 00:17:58 +0900

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,31 @@
 require "spec_helper"
 require "serverspec"
 
-package = "acme_client"
-service = "acme_client"
-config  = "/etc/acme_client/acme_client.conf"
-user    = "acme_client"
-group   = "acme_client"
-ports   = [PORTS]
-log_dir = "/var/log/acme_client"
-db_dir  = "/var/lib/acme_client"
+config  = "/etc/acme-client.conf"
+key_dir = "/etc/acme"
+default_group = "wheel"
 
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/acme_client.conf"
-  db_dir = "/var/db/acme_client"
+describe file key_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by "root" }
+  it { should be_grouped_into default_group }
+  it { should be_mode 700 }
 end
 
-describe package(package) do
-  it { should be_installed }
-end
-
-describe file(config) do
+describe file config do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("acme_client") }
+  it { should be_owned_by "root" }
+  it { should be_grouped_into default_group }
+  it { should be_mode 644 }
+  its(:content) { should match(/Managed by ansible/) }
+  its(:content) { should match(/^authority letsencrypt {$/) }
+  its(:content) { should match(/^domain example\.org {$/) }
+  its(:content) { should match(/^domain example\.net {$/) }
 end
 
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/acme_client") do
-    it { should be_file }
-  end
-end
-
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+describe cron do
+  it { should have_entry "45 8 * * * acme-client -f /etc/acme-client.conf -vv example.org" }
+  it { should have_entry "45 9 * * * acme-client -f /etc/acme-client.conf -vv example.net" }
 end
