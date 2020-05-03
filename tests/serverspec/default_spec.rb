b8138c840d2a3d749b958972f0e47448e40984e2
Tomoyuki Sakurai <y@trombik.org>
2020-05-04 00:28:34 +0900

    bugfix: support multiple challengedir directories

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -4,6 +4,25 @@ require "serverspec"
 config  = "/etc/acme-client.conf"
 key_dir = "/etc/acme"
 default_group = "wheel"
+challengedirs = ["/var/www/acme"]
+
+describe file key_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by "root" }
+  it { should be_grouped_into default_group }
+  it { should be_mode 700 }
+end
+
+challengedirs.each do |d|
+  describe file d do
+    it { should exist }
+    it { should be_directory }
+    it { should be_owned_by "root" }
+    it { should be_grouped_into "daemon" }
+    it { should be_mode 755 }
+  end
+end
 
 describe file key_dir do
   it { should exist }
